5ea40a693d4e9cf22aa5979cda00a52bc211203d
WQ <wcq0216@163.com>
2025-03-14 11:18:42 +0800

    JSX语法，props，state，触发事件

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import Todo from "./components/Todo";
+import "./App.css";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
+function App(props) {
+  // 添加任务
+  // 初始为index.js传递过来的
+  const [tasks, setTasks] = useState(props.tasks);
+  // form表单提交后，子组件触发事件传递name过来，再修改tasks的值，tasks发生变化后触发页面重新渲染
+  function addTask(name) {
+    // 制定唯一标识符
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+    console.log("tasks", tasks);
+  }
+  const taskList = tasks?.map((task) => (
+    <Todo
+      name={task.name}
+      id={task.id}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton name="all" />
+        <FilterButton name="all1" />
+        <FilterButton name="all3" />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
