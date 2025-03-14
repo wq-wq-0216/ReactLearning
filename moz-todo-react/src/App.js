da24af00faa8871a0a87d53220cbc7fc8b1adcf5
WQ <wcq0216@163.com>
2025-03-14 14:39:30 +0800

    实现了添加任务、删除任务和将任务切换为已完成的功能

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -8,7 +8,7 @@ import { nanoid } from "nanoid";
 
 function App(props) {
   // 添加任务
-  // 初始为index.js传递过来的
+  // tasks初始为index.js传递过来的
   const [tasks, setTasks] = useState(props.tasks);
   // form表单提交后，子组件触发事件传递name过来，再修改tasks的值，tasks发生变化后触发页面重新渲染
   function addTask(name) {
@@ -17,14 +17,41 @@ function App(props) {
     setTasks([...tasks, newTask]);
     console.log("tasks", tasks);
   }
+  // 任务列表
   const taskList = tasks?.map((task) => (
     <Todo
       name={task.name}
       id={task.id}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
+  // 标题文字
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  // 将复选框与浏览器中的状态统一
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    // 更新任务复选框状态
+    setTasks(updatedTasks);
+    console.log("tasks", taskList);
+  }
+  // 删除任务
+  function deleteTask(id) {
+    console.log("id", id);
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -34,7 +61,7 @@ function App(props) {
         <FilterButton name="all1" />
         <FilterButton name="all3" />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
